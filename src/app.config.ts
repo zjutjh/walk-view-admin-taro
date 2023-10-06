e70338d797a62afd69d27c29069433c891423254
王天赐 <tiancy@wangtiansis-MacBook-Air.local>
2023-10-06 16:46:26 +0800

    fix(filename):修改文件命名

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,7 @@
 export default {
   pages: [
     "pages/index/index",
-    "pages/manulMember/index",
+    "pages/handleScanMember/index",
     "pages/scanMember/index",
     "pages/scanTeam/index",
     "pages/teamInfo/index",
