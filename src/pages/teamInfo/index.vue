27a67241e296099f1dc805013855e80d33509c7e
Patrickill <1322301665@qq.com>
2023-10-09 15:01:31 +0800

    fix: 修复跳转bug

--- src/pages/teamInfo/index.vue
+++ src/pages/teamInfo/index.vue
@@ -58,13 +58,32 @@ import {onMounted, ref} from "vue";
 import {TeamStatus} from "../../types/teamStatus";
 import {memberStorageType, useMembersStore} from "../../stores/members";
 import {getTeamStatus} from "../../services/services/teamService";
-import Taro from "@tarojs/taro";
+import Taro, {useDidShow} from "@tarojs/taro";
 const teamStatus: string[] = ["未开始","未开始","进行中","扫码成功","放弃","完成"];
 const route: string[] = ["朝晖路线","屏峰半程","屏峰全程","莫干山半程","莫干山全程"];
 const walkStatus: string[] = ["未处理","以扫码放行","已放弃"];
 const membersStore = useMembersStore();
 const members = ref<memberStorageType[]>();
 const teamData = ref<TeamStatus | boolean >();
+
+useDidShow(async () => {
+  const data = {
+    team_id: 1
+  };
+  teamData.value = await getTeamStatus(data);
+  if(!teamData.value) {
+    await Taro.showModal({
+      title: "获取团队信息失败!",
+      content: "权限不够或者登录有误!"
+    });
+    await Taro.navigateTo({
+      url: "/pages/scanTeam/index"
+    });
+  }
+  membersStore.initMembers(teamData.value["member"]);
+  members.value = membersStore.getMembers();
+  console.log(members);
+});
 
 onMounted(async () => {
   // teamData.value = ref<TeamStatus | boolean >({ "admin": {
